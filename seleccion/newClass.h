84d852f858a8251a499edd3cc6feb8d84a669860
MichaelleValenzuela <58209320+MichaelleValenzuela@users.noreply.github.com>
2019-11-28 10:50:16 -0500

    Add files via upload

---
+++ seleccion/newClass.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newClass.h
+ * Author: Algoritmos
+ *
+ * Created on 28 de noviembre de 2019, 9:13
+ */
+
+#ifndef NEWCLASS_H
+#define NEWCLASS_H
+
+class newClass {
+public:
+    newClass();
+    newClass(const newClass& orig);
+    virtual ~newClass();
+private:
+
+};
+
+#endif /* NEWCLASS_H */
+
